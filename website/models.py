6d9e4fce3d05c6df8b54b277dc9d7bdff2faaa17
nikeshh-x <nikeshmanandhar16@gmail.com>
2025-12-28 17:49:07 +0545

    Teams Section

--- website/models.py
+++ website/models.py
@@ -40,5 +40,26 @@ class GalleryMedia(models.Model):
     def __str__(self):
         return self.title
     
+class TeamMember(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='team/photos/', blank=True, null=True)
+    order = models.PositiveIntegerField(default=0, help_text="Lower numbers appear first")
+
+    linkedin_url = models.URLField(max_length=200, blank=True)
+    instagram_url = models.URLField(max_length=200, blank=True)
+    facebook_url = models.URLField(max_length=200, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['order', 'name']
+        verbose_name = 'Team Member'
+        verbose_name_plural = 'Team Members'
+    
     
     
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Partner, GalleryMedia
+from .models import Partner, GalleryMedia, TeamMember
 
 # Create your views here.
 
@@ -9,9 +9,14 @@ def home(request):
 
     # Get all gallery media to display on the home page
     gallery_items = GalleryMedia.objects.all()[:6]
+    
+    # Get all team members to display on the home page
+    teams = TeamMember.objects.all()
+    
     context = {
         'partners':partners,
         'gallery_items':gallery_items,
+        'teams':teams,
     }
     return render(request, 'website/home.html', context)
 
